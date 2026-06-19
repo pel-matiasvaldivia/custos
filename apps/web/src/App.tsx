958d44a31987203a10dfccf4af3a44b79c682d94
Matias Valdivia <matiasvaldivia@pymesenlinea.com.ar>
2026-06-19 09:05:59 -0300

    Fix: Unused React import in App.tsx (fixing CI build)

--- apps/web/src/App.tsx
+++ apps/web/src/App.tsx
@@ -1,4 +1,3 @@
-import React from 'react';
 import { Routes, Route } from 'react-router-dom';
 import { Sidebar } from './components/layout/Sidebar';
 import { PersonnelPage } from './pages/personnel/PersonnelPage';
